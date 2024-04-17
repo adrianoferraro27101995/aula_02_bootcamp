86cea53ee2788af1242206a6e4da4b946481bc02
adrianoferraro27101995 <adriano.ferraro27101995@gmail.com>
2024-04-17 18:43:26 -0300

    adicionado exercicio 14

--- exercicios.py
+++ exercicios.py
@@ -28,6 +28,17 @@ print(f"{area_do_circulo:.2f}")
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+data = input("Insira uma data no formato dd/mm/aaaa:")
+
+dia = data.split("/")[0]
+mes = data.split("/")[1]
+ano = data.split("/")[2]
+
+print(f"O dia é {dia}")
+print(f"O mês é {mes}")
+print(f"O ano é {ano}")
+
+
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
 # #### Booleanos (`bool`)
